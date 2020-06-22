308c4d3d5aed41809590a7a9e834ceca724cf198
luke williams <jluke94williams@gmail.com>
2020-06-22 14:12:54 -0400

    Adding Navbar

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -4,9 +4,9 @@ import {Link} from "react-router-dom";
 function Nav() {
     return(
         <nav>
-            <div class="nav-wrapper">
-                <Link to='/' class="brand-logo center">Google Book Search</Link> 
-                    <ul id="nav-mobile" class="left hide-on-med-and-down">
+            <div className="nav-wrapper">
+                <Link to='/' className="brand-logo center">Google Book Search</Link> 
+                    <ul id="nav-mobile" className="left hide-on-med-and-down">
                         <li><Link to='/'>Search</Link></li>
                         <li><Link to='/saved'>Saved</Link></li>
                     </ul>
